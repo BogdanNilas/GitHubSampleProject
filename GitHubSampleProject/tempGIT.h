ad771e95376482f39595c5759cbd83ae11cd08b7
BogdanNilas <nbogdan86@gmail.com>
2014-01-23 00:46:53 +0100

    a
    a

---
+++ GitHubSampleProject/tempGIT.h
@@ -0,0 +1,13 @@
+//
+//  tempGIT.h
+//  GitHubSampleProject
+//
+//  Created by Bogdan Nilas on 23/01/14.
+//  Copyright (c) 2014 Bogdan Nilas. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface tempGIT : NSObject
+
+@end
---
+++ GitHubSampleProject/tempGIT.m
@@ -0,0 +1,13 @@
+//
+//  tempGIT.m
+//  GitHubSampleProject
+//
+//  Created by Bogdan Nilas on 23/01/14.
+//  Copyright (c) 2014 Bogdan Nilas. All rights reserved.
+//
+
+#import "tempGIT.h"
+
+@implementation tempGIT
+
+@end
