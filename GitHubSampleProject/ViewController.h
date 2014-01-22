a6aecfc523a4704afe601fe2619cd2f615b81d2c
BogdanNilas <nbogdan86@gmail.com>
2014-01-23 00:24:54 +0100

    Initial Commit

---
+++ GitHubSampleProject/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GitHubSampleProject
+//
+//  Created by Bogdan Nilas on 23/01/14.
+//  Copyright (c) 2014 Bogdan Nilas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ GitHubSampleProject/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  GitHubSampleProject
+//
+//  Created by Bogdan Nilas on 23/01/14.
+//  Copyright (c) 2014 Bogdan Nilas. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
